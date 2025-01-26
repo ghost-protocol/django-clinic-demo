d070d24db66fe49099d6b49c721ccf9685ce56cd
Kay <kwamedavour@gmail.com>
2025-01-26 05:11:13 -0500

    working base

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # django-clinic-demo
+# To Do
+use environment variable to replace default django secret key in SETTINGS
---
+++ clinic/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic.base import TemplateView
+
+
+class HomeTemplateView(TemplateView):
+    template_name = "index.html"
+    
+# def home(request):
+#     return HttpResponse('Working!!!')
