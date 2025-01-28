f6594dcd8898b279b2afe4ddb20db58e21b10063
Kay <kwamedavour@gmail.com>
2025-01-28 15:57:43 -0500

    complete layout and templates

--- clinic/urls.py
+++ clinic/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import HomeTemplateView, ContactTemplateView
+from .views import HomeTemplateView, ContactTemplateView, AboutTemplateView, ServicesTemplateView, AppointmentTemplateView
 
 
 urlpatterns = [
     path('', HomeTemplateView.as_view(), name='home'),
+    path('about', AboutTemplateView.as_view(), name='about'),
+    path('services', ServicesTemplateView.as_view(), name='services'),
+    path('appointment', AppointmentTemplateView.as_view(), name='appointment'),
     path('contact', ContactTemplateView.as_view(), name='contact'),
 ]
--- clinic/views.py
+++ clinic/views.py
@@ -2,9 +2,9 @@ import datetime
 
 from django.conf import settings
 from django.contrib import messages
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.core.mail import EmailMessage
 
 from django.views.generic.base import TemplateView
@@ -13,6 +13,13 @@ from django.views.generic.base import TemplateView
 
 class HomeTemplateView(TemplateView):
     template_name = "index.html"
+
+class AboutTemplateView(TemplateView):
+    template_name = "about.html"
+
+class ServicesTemplateView(TemplateView):
+    template_name = "services.html"
+    
 
 class ContactTemplateView(TemplateView):
     template_name = "contact.html"
@@ -30,7 +37,36 @@ class ContactTemplateView(TemplateView):
         #     reply_to=[email]
         # )
         # email.send()
-        return HttpResponse("Email sent successfully!")
-    
-# def home(request):
-#     return HttpResponse('Working!!!')
+
+        messages.add_message(request, messages.SUCCESS, f"Thanks {name} for contacting us. Our agents will respond to you ASAP")
+        return HttpResponseRedirect(request.path)
+        # messages.success(request, 'Email submitted successfully!')
+        # return HttpResponse("Email sent successfully!")
+        # return redirect(request.META.get('HTTP_REFERER', '/'))  # Redirect to the previous page or '/' if not available
+
+
+class AppointmentTemplateView(TemplateView):
+    template_name = "appointment.html"
+
+    def post(self, request):
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        mobile = request.POST.get("mobile")
+        doctor = request.POST.get("doctor")
+        date = request.POST.get("date")
+        time = request.POST.get("time")
+        problem = request.POST.get("problem")
+
+        # email = EmailMessage(
+        #     subject= f"{name} from doctor family.",
+        #     body=message,
+        #     from_email=settings.EMAIL_HOST_USER,
+        #     to=[settings.EMAIL_HOST_USER],
+        #     reply_to=[email]
+        # )
+        # email.send()
+
+        print(name, email, mobile, doctor, date+time, problem)
+
+        messages.add_message(request, messages.SUCCESS, f"Thanks {name} for booking an appointment for {date, time}. Our agents will respond to you ASAP")
+        return HttpResponseRedirect(request.path)
